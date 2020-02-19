c6b0ce0e51e42ac39d48fed97e16b285657c40a5
Uriah Michaeli <uriahmic@gmail.com>
2020-02-19 19:29:07 +0200

    projectile script (Itay)

---
+++ Scripts/ProjectileScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileScript : MonoBehaviour
+{
+    public GameObject player;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Portal")
+        {
+            {
+                Transform tp = collision.gameObject.transform;
+                player.transform.position = tp.position;
+                Destroy(gameObject);
+            }
+        }
+    }
+}
