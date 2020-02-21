abba72269c2776e6b4c92c5e6045b5704d97adfe
FlakaShlaka <flakashlaka@gmail.com>
2020-02-21 11:03:39 +0200

    oris shit

---
+++ Scripts/ProjectileOri.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed;
+    public float lifeTime;
+    public float distance;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("DestroyProjectile", lifeTime);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.up * speed * Time.deltaTime);
+
+    }
+
+    void DestroyProjectile()
+    {
+        Destroy(gameObject);
+    }
+
+
+
+}
